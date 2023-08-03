8f58f3b7dddd9c3cf2f6a21bc17482d18828d5ca
ArtistHam <great.artist.ham@gmail.com>
2023-08-03 20:14:50 +0300

    Last commit

--- main.js
+++ main.js
@@ -22,11 +22,28 @@ const start = () => {
   const input = document.getElementById("guess");
   const btn = document.getElementById("guessBtn");
   const table = document.getElementById("history");
+  const score = document.getElementById("score");
+  const high_score = document.getElementById("highscore");
 
   let history = [];
 
+  const highScore = localStorage.getItem("highScore");
+
+  if (highScore) {
+    high_score.innerText = highScore;
+  }
+
+  let currentScore = 1000 * numberOfDigits;
+
+  score.innerText = currentScore;
+
   btn.addEventListener("click", () => {
-    if (+input.value.length === +numberOfDigits && input.value.match(/\d/)) {
+    if (
+      +input.value.length === +numberOfDigits &&
+      input.value.match(/\d/) &&
+      !input.value.match(/0/) &&
+      checkUnique(+input.value)
+    ) {
       const tr = document.createElement("tr");
       const td1 = document.createElement("td");
       const td2 = document.createElement("td");
@@ -36,6 +53,12 @@ const start = () => {
       ]);
 
       if (!history.includes(input.value)) {
+        if (currentScore > 10 * history.length) {
+          currentScore = currentScore - 10 * history.length;
+        }
+
+        score.innerText = currentScore;
+
         history.push(input.value);
 
         td1.innerText = input.value;
@@ -45,17 +68,32 @@ const start = () => {
         tr.appendChild(td2);
 
         table.appendChild(tr);
+        input.value = "";
       } else {
         alert("This number already used");
       }
     } else {
-      alert(`You should enter number with ${numberOfDigits} digits!`);
+      alert(
+        `You should enter number with ${numberOfDigits} unique non-zero digits!`
+      );
     }
   });
 };
 
 const getRandomIndex = () => {
   return +Math.random().toFixed(1) * 10;
+};
+
+const checkUnique = (number) => {
+  let arr = `${number}`.split("");
+  let uniqueSet = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (uniqueSet.includes(arr[i])) {
+      return false;
+    }
+    uniqueSet.push(arr[i]);
+  }
+  return true;
 };
 
 const juniorsAndSeniorsAlgorithm = (answer, guess) => {
@@ -73,6 +111,8 @@ const juniorsAndSeniorsAlgorithm = (answer, guess) => {
   }
 
   if (seniors === answer.length) {
+    const score = document.getElementById("score");
+    localStorage.setItem("highScore", +score.innerText);
     alert("You win!");
     location.reload();
   }
